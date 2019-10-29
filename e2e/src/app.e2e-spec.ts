eb86d7d77cb309eda9b41356eccd7d44c5af8949
Tharinduyasarathna27 <tharinduyasarathna@gmail.com>
2019-10-29 14:22:16 +0530

    minor fix

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to angular6-sidenav-example!');
+    expect(page.getParagraphText()).toEqual('Welcome to smart-eye!');
   });
 });
